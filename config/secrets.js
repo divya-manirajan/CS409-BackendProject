ba69edd2d27377d11cf6586c57d9410eba7f1a29
divya-manirajan <divya.manirajan@gmail.com>
2025-02-10 16:28:05 -0600

    added gitignore

--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://divyamanirajan:qxXnmO2u7wnJZqAN@cluster0.1xcdsnq.mongodb.net/test"
-    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
-};
